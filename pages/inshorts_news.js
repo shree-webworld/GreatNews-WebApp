c64aa12f063de5b219aad661950bdc6ac2ddd0ef
Shreedhar Sawant <shreedharsawant3@gmail.com>
2022-11-08 13:16:19 +0530

    updated api url.

--- pages/inshorts_news.js
+++ pages/inshorts_news.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Link from 'next/link';
 import {useForm} from 'react-hook-form';
 import Preloader from '../components/Preloader';
-
+import Image from 'next/image';
 
 export default function Inshorts_News()
 {
@@ -11,7 +11,7 @@ export default function Inshorts_News()
   let [news, setNews] = useState([]);
   const { register, handleSubmit } = useForm();
   const [loading, setLoading] = useState(false);
- 
+
 
   let getSelectedNews =
       async(data) =>{
@@ -19,10 +19,10 @@ export default function Inshorts_News()
                      { //console.log(`The value is `+data);//The value is [object Object]
                       let category_value = Object.values(data);
 
-                      await axios.get(`https://inshortsv2.vercel.app/news?type=${category_value}`)
+                      await axios.get(`https://inshorts.me/news/topics/${category_value}`)
                        .then((response) =>{
                                               //console.log(response.data.category);
-                                              setNews(response.data.articles);
+                                              setNews(response.data.data.articles);
                                               setLoading(true);
                                           }
                             )
@@ -42,10 +42,10 @@ export default function Inshorts_News()
                     async() =>{
                                   try
                                    {
-
-                                     await axios.get(`https://inshortsv2.vercel.app/news?type=all_news`)
+                                     await axios.get(`https://inshorts.me/news/top?offset=0&limit=10`)
                                      .then((response) =>{
-                                                            setNews(response.data.articles);
+                                                            //console.log("inshorts",response.data.data.articles);
+                                                            setNews(response.data.data.articles);
                                                             setLoading(true);
                                                         }
                                           )
@@ -76,9 +76,7 @@ export default function Inshorts_News()
       <form onSubmit={handleSubmit(getSelectedNews)} className="flex flex-row space-x-3">
       <select name="news_categories" defaultValue={'DEFAULT'} {...register("news_categories")} className="select select-primary w-full max-w-xs selectCategory">
         <option value="DEFAULT" disabled >Select the News Category</option>
-        <option value="all_news">General</option>
-        <option value="trending">Trending</option>
-        <option value="top_stories">Top Stories</option>
+        <option value="all">General</option>
         <option value="national">National</option>
         <option value="business">Business</option>
         <option value="sports">Sports</option>
@@ -107,17 +105,17 @@ export default function Inshorts_News()
           loading ?
         (  news.map((currentElement,index) =>{
                                               return(
-                                                      <div className="p-4 md:w-1/3" key = {index}>
+                                                      <div className="p-4 md:w-1/3" key = {currentElement.hashId}>
                                                        <div className="card w-96 h-full bg-indigo-100 shadow-lg shadow-indigo-300">
                                                         <figure className="px-10 pt-10 flex flex-col">
-                                                          <img src={currentElement.image_url} alt="News articles" className="rounded-xl mb-2" />
-                                                          <figcaption className="tracking-widest text-xs title-font font-medium text-white badge badge-primary badge-md">Source: {currentElement.source_name}</figcaption>
+                                                          <img src={currentElement.imageUrl} alt="News articles" className="rounded-xl mb-2" />
+                                                          <figcaption className="tracking-widest text-xs title-font font-medium text-white badge badge-primary badge-md">Source: {currentElement.sourceName}</figcaption>
                                                         </figure>
                                                         <div className="card-body ">
                                                           <h2 className="card-title text-lg font-bold text-gray-900" style={{fontFamily: "'Montserrat', sans-serif"}}>{currentElement.title}</h2>
-                                                          <p>{currentElement.description}</p>
+                                                          <p>{currentElement.subtitle}</p>
                                                           <div className="grid grid-cols-2">
-                                                            <Link href={currentElement.source_url}>
+                                                            <Link href={currentElement.shortenedUrl}>
                                                               <a className="link link-primary link-hover inline-flex items-center md:mb-2 lg:mb-0" target="_blank">Learn More ➜</a>
                                                             </Link>
                                                             <span></span>
